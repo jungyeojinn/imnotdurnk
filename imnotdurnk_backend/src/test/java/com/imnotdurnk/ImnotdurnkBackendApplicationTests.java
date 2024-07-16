a2c23bd4fbc3968124d8ce915c3207fb502d6de8
yngbao97 <yukyj0418@gmail.com>
2024-07-16 13:19:21 +0900

    S11P12A609-113 init: Backend 프로젝트 생성 및 최상위 디렉토리 초기화

---
+++ imnotdurnk_backend/src/test/java/com/imnotdurnk/ImnotdurnkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imnotdurnk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImnotdurnkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
