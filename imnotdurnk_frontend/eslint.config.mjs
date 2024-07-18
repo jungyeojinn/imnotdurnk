1a201f9bcdc27271f358babd800216f4a4413671
박다솔 <padasol@naver.com>
2024-07-18 17:40:18 +0900

    Revert "Merge branch 'feature/back/auth' into 'main'"
    This reverts merge request !3

--- imnotdurnk_frontend/eslint.config.mjs
+++ imnotdurnk_frontend/eslint.config.mjs
@@ -1,12 +1,12 @@
-import pluginJs from '@eslint/js';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
 import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 import importPlugin from 'eslint-plugin-import';
 import reactHooksPlugin from 'eslint-plugin-react-hooks';
-import reactNativePlugin from 'eslint-plugin-react-native';
 import reactRefreshPlugin from 'eslint-plugin-react-refresh';
+import reactNativePlugin from 'eslint-plugin-react-native';
 
 export default [
     {
@@ -26,7 +26,7 @@ export default [
             'react-native': reactNativePlugin,
         },
         rules: {
-            // 1) React 관련 규칙
+            // React 관련 규칙
             'react-refresh/only-export-components': [
                 'warn',
                 { allowConstantExport: true },
@@ -34,17 +34,19 @@ export default [
             'react/react-in-jsx-scope': 'off', // React 17 이후 필요 없음
             'react/prop-types': 'off', // PropTypes 비활성화 (필요 시 활성화)
 
-            // 2) React Native 관련 규칙
+            // React Native 관련 규칙
             'react-native/no-unused-styles': 'warn',
+            'react-native/split-platform-components': 'warn',
             'react-native/no-inline-styles': 'warn',
             'react-native/no-color-literals': 'warn',
             'react-native/no-raw-text': 'warn',
+            'react-native/no-single-element-style-arrays': 'warn',
 
-            // 3) Hooks 관련 규칙
+            // Hooks 관련 규칙
             'react-hooks/rules-of-hooks': 'error', // Hooks 규칙 강제
             'react-hooks/exhaustive-deps': 'warn', // 의존성 배열 관리
 
-            // 4) import 관련 규칙
+            // import 관련 규칙
             'import/named': 'error',
             'import/default': 'error',
             'import/namespace': 'error',
@@ -61,7 +63,7 @@ export default [
             'import/no-duplicates': 'warn',
             'import/first': 'warn',
 
-            // 5) 변수 처리 규칙
+            // 변수 처리 규칙
             'no-use-before-define': 'error', // 변수를 정의하기 전에 사용하면 오류
             'no-unused-vars': [
                 'warn',
@@ -69,7 +71,7 @@ export default [
             ],
             'no-shadow': ['warn'],
 
-            // 6) 기타 규칙
+            // 기타 규칙
             eqeqeq: 'warn', // // 엄격한 일치 연산자 사용 (===와 !==를 사용)
             curly: 'warn', // 제어문에 중괄호 사용
             'arrow-parens': ['warn', 'always'], // 화살표 함수에 파라미터 괄호처리
