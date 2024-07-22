c305c61717f9a2a57840ac2305f111ff2db31497
박다솔 <ds10x2@gmail.com>
2024-07-22 15:25:00 +0900

    S11P12A609-175 feat: 토큰 검증 인터셉터  Config 작성

---
+++ imnotdurnk_backend/src/main/java/com/imnotdurnk/global/config/AuthConfig.java
@@ -0,0 +1,29 @@
+package com.imnotdurnk.global.config;
+
+import com.imnotdurnk.global.interceptor.JwtInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AuthConfig implements WebMvcConfigurer {
+
+    private final JwtInterceptor jwtTokenInterceptor;
+
+    public AuthConfig(JwtInterceptor jwtTokenInterceptor) {
+        this.jwtTokenInterceptor = jwtTokenInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        /**
+         * 토큰 인증에서 제외
+         * 1. 로그인
+         * 2. 로그아웃
+         * 3. 회원가입
+         * 4. Swagger UI
+         */
+        registry.addInterceptor(jwtTokenInterceptor)
+                .excludePathPatterns("/users/login", "/users/signup/**", "/swagger-ui/**");
+    }
+}
