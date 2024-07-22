9a8b9fe4b3721d6f7470fa86067a0e807b24d06c
박다솔 <ds10x2@gmail.com>
2024-07-22 17:21:15 +0900

    S11P12A609-165 feat: 음주 일정 기록 피드백 작성 API 작성

--- imnotdurnk_backend/src/main/java/com/imnotdurnk/domain/calendar/controller/CalendarController.java
+++ imnotdurnk_backend/src/main/java/com/imnotdurnk/domain/calendar/controller/CalendarController.java
@@ -1,4 +1,37 @@
 package com.imnotdurnk.domain.calendar.controller;
 
+import com.imnotdurnk.domain.calendar.dto.CalendarDto;
+import com.imnotdurnk.domain.calendar.entity.CalendarEntity;
+import com.imnotdurnk.domain.calendar.service.CalendarService;
+import lombok.AllArgsConstructor;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Calendar;
+
+@RestController
+@RequestMapping("/calendars")
+@AllArgsConstructor
 public class CalendarController {
+
+    private CalendarService calendarService;
+
+    @PutMapping("/{date}/plans/{planId}")
+    public ResponseEntity<?> feedbackPlan(@RequestAttribute(value = "AccessToken", required = true) String accessToken,
+                                          @PathVariable String date,
+                                          @PathVariable int planId,
+                                          @RequestBody CalendarDto calendarDto) throws BadRequestException {
+
+        //피드백 등록
+        CalendarEntity calendarEntity = calendarService.feedbackPlan(accessToken, date, planId, calendarDto);
+
+        //수정이 되지 않은 경우
+        if (calendarEntity == null) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+        //수정이 완료된 경우
+        else return ResponseEntity.status(HttpStatus.OK).build();
+    }
 }
--- imnotdurnk_backend/src/main/java/com/imnotdurnk/domain/calendar/service/CalendarService.java
+++ imnotdurnk_backend/src/main/java/com/imnotdurnk/domain/calendar/service/CalendarService.java
@@ -1,4 +1,10 @@
 package com.imnotdurnk.domain.calendar.service;
 
+import com.imnotdurnk.domain.calendar.dto.CalendarDto;
+import com.imnotdurnk.domain.calendar.entity.CalendarEntity;
+import com.imnotdurnk.global.exception.ResourceNotFoundException;
+import org.apache.coyote.BadRequestException;
+
 public interface CalendarService {
+    CalendarEntity feedbackPlan(String accessToken, String date, int planId, CalendarDto calendarDto) throws BadRequestException, ResourceNotFoundException;
 }
