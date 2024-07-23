16612cbe440dcc601d3f589078e4413babda9dca
박다솔 <ds10x2@gmail.com>
2024-07-23 14:08:07 +0900

    S11P12A609-168 feat: 필요한 커스텀 예외 작성 및 Http Status Code 추가

--- imnotdurnk_backend/src/main/java/com/imnotdurnk/global/exception/InvalidTokenException.java
+++ imnotdurnk_backend/src/main/java/com/imnotdurnk/global/exception/InvalidTokenException.java
@@ -1,8 +1,21 @@
 package com.imnotdurnk.global.exception;
 
-/**
- * 받은 토큰이 유효하지 않은 경우 발생하는 exception
- */
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-public class InvalidTokenException  extends Exception{
+/**
+ * 받은 토큰이 유효하지 않은 경우 UNAUTHORIZED 401를 반환
+ * 401: 요청된 리소스에 대한 유효한 인증 자격 증명이 없기 때문에 클라이언트 요청이 완료되지 않았음을 나타냅니다.
+ */
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class InvalidTokenException  extends RuntimeException{
+    private int code = HttpStatus.UNAUTHORIZED.value();
+
+    public InvalidTokenException(String message) {
+        super(message);
+    }
+
+    public int getCode() {
+        return code;
+    }
 }
