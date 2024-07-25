671695b4c4fd01fa494dd344e2151b23145b1f3d
unknown <jeongsangyoung@naver.com>
2024-07-25 15:43:31 +0900

    S11P12A609-153 feat: 비밀번호 인풋 박스 추가 구현

--- imnotdurnk_frontend_web/src/components/_common/InputBox.jsx
+++ imnotdurnk_frontend_web/src/components/_common/InputBox.jsx
@@ -1,18 +1,47 @@
+import { useState } from 'react';
 import * as St from './InputBox.style';
 
-const InputBox = ({ labelText, iconName, inputType, size }) => {
+const InputBox = ({ labelText, iconName, inputType, size, autocomplete }) => {
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+    // 가시성 설정 토글
+    const handleToggleVisibility = (event) => {
+        event.preventDefault();
+        setIsPasswordVisible(!isPasswordVisible);
+    };
+
+    // password만 분기 적용
+    const currentInputType =
+        inputType === 'password' && isPasswordVisible ? 'text' : inputType;
+
     return (
         <St.InputBoxContainer $size={size}>
             <St.TextContainer>
                 <St.InputLabel>{labelText}</St.InputLabel>
-                <St.Input />
+                <St.Input type={currentInputType} autoComplete={autocomplete} />
             </St.TextContainer>
-            <St.InputIcon
-                src={`src/assets/icons/size_24/Icon-${iconName}.svg`}
-                alt={`${iconName} icon`}
-                type={inputType}
-                $isEmpty={iconName === 'empty'}
-            />
+
+            {inputType === 'password' ? (
+                <St.InputIcon
+                    as="button" // 버튼처럼 사용
+                    onClick={handleToggleVisibility}
+                >
+                    <img
+                        src={`src/assets/icons/size_24/Icon-${isPasswordVisible ? 'visible' : 'invisible'}.svg`}
+                        alt={
+                            isPasswordVisible
+                                ? 'Hide password'
+                                : 'Show password'
+                        }
+                    />
+                </St.InputIcon>
+            ) : (
+                <St.InputIcon
+                    src={`src/assets/icons/size_24/Icon-${iconName}.svg`}
+                    alt={`${iconName} icon`}
+                    $isEmpty={iconName === 'empty'}
+                />
+            )}
         </St.InputBoxContainer>
     );
 };
