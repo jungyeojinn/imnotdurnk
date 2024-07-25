b61df242319334cb081e57d8b70309caf9d1236d
이나연 <sh05060@naver.com>
2024-07-25 16:51:52 +0900

    S11P12A609-133 feat: 회원가입 레이아웃 구현

---
+++ imnotdurnk_frontend_web/src/components/signup/Signup.style.js
@@ -0,0 +1,24 @@
+import { styled } from 'styled-components';
+
+export const SignupContainer = styled.div`
+    display: flex;
+    width: 329px;
+    padding: 0px 33px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    gap: 19px;
+`;
+
+export const SubTitle = styled.h3`
+    color: var(----color-green3, #252f2c);
+
+    font-size: 14px;
+    line-height: normal;
+`;
+export const MainTitle = styled.h1`
+    color: var(----color-green3, #252f2c);
+
+    font-size: 32px;
+    line-height: normal;
+`;
