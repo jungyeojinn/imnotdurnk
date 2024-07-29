79106c2f7ffd98e14bd93106a5a946f55ab0d4e4
박다솔 <ds10x2@gmail.com>
2024-07-29 13:09:12 +0900

    S11P12A609-109 feat: CORS 설정

---
+++ imnotdurnk_backend/src/main/java/com/imnotdurnk/global/config/CORSConfig.java
@@ -0,0 +1,26 @@
+package com.imnotdurnk.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry
+                .addMapping("/**") // 허용하려는 API 요청 경로
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용하려는 HTTP Method 설정 (OPTIONS는 Preflight 설정)
+                .allowedOrigins("http://localhost:5174/", "http://localhost:8081/", "http://localhost:5554/") // 허용하려는 클라이언트 측 주소
+                .allowCredentials(true) // HttpOnly Cookie를 사용하기 위한 설정
+                .exposedHeaders("Access-Control-Allow-Headers")
+                .exposedHeaders("Authorization") // 클라이언트에서 해당 Header의 값을 확인할 수 있도록 설정.
+                .maxAge(3600); // Preflight Cache 설정
+
+        WebMvcConfigurer.super.addCorsMappings(registry);
+    }
+
+
+}
+
