0e908a7b429fa120813b340b3d52b110d6585c67
jeongsangyoung <jeongsangyoung@naver.com>
2024-07-31 13:17:09 +0900

    S11P12A609-230 feat: 출발지 및 목적지 검색 기능, 현재 위치 지도에 표시 구현

--- imnotdurnk_frontend_mobile/pages/Map.jsx
+++ imnotdurnk_frontend_mobile/pages/Map.jsx
@@ -1,14 +1,19 @@
-import { useState, useEffect } from 'react';
 import * as Location from 'expo-location';
-import useNavigationStore from '../stores/useNavigationStore';
-import SearchBar from '../components/map/SearchBar';
-import CustomMap from '../components/map/CustomMap';
+import { useEffect, useState } from 'react';
+import IconButton from '../components/_common/IconButton';
 import * as St from '../components/_layout/globalStyle';
+import CustomMap from '../components/map/CustomMap';
+import SearchBar from '../components/map/SearchBar';
+import { getReverseGeocoding } from '../services/map';
+import useNavigationStore from '../stores/useNavigationStore';
 
 const Map = () => {
     const { setNavigation } = useNavigationStore();
-    const [region, setRegion] = useState(null);
-    const [currentAddress, setCurrentAddress] = useState('');
+    const [region, setRegion] = useState(null); // 지도에 보여주는 좌표
+    const [departure, setDeparture] = useState(null); // 출발지 좌표
+    const [destination, setDestination] = useState(null); // 목적지 좌표
+    const [departurePlaceholder, setDeparturePlaceholder] =
+        useState('출발지를 입력하세요');
 
     useEffect(() => {
         setNavigation({
@@ -18,40 +23,111 @@ const Map = () => {
             icon2: { iconname: 'empty', isRed: false },
         });
 
-        // 위치 정보 수집 권한 받기
+        // 위치 정보 수집 권한 요청 및 초기 위치 설정
         const getLocationPermission = async () => {
             try {
                 let { status } =
                     await Location.requestForegroundPermissionsAsync();
                 if (status !== 'granted') {
-                    console.log('Location permission not granted');
+                    console.log('위치 권한이 허용되지 않았습니다.');
                     return;
                 }
 
                 let location = await Location.getCurrentPositionAsync({});
                 const { latitude, longitude } = location.coords;
 
+                // 초기 위치를 region과 출발지로 설정
                 setRegion({
-                    latitude,
-                    longitude,
-                    latitudeDelta: 0.01,
-                    longitudeDelta: 0.01,
+                    latitude: 37.50127843458193,
+                    longitude: 127.0396046598167,
+                    latitudeDelta: 0.005,
+                    longitudeDelta: 0.005,
+                });
+
+                setDeparture({
+                    latitude: 37.50127843458193,
+                    longitude: 127.0396046598167,
                 });
             } catch (error) {
-                console.error('Error getting location permission', error);
+                console.error('위치 권한 요청 중 오류 발생', error);
             }
         };
 
         getLocationPermission();
     }, [setNavigation]);
 
+    useEffect(() => {
+        if (region) {
+            // 역 지오코딩을 사용하여 주소 얻기
+            const fetchAddress = async () => {
+                try {
+                    const address = await getReverseGeocoding(
+                        region.latitude,
+                        region.longitude,
+                    );
+                    setDeparturePlaceholder(address);
+                } catch (error) {
+                    console.error('주소 요청 중 오류 발생', error);
+                }
+            };
+
+            fetchAddress();
+        }
+    }, [region]);
+
+    // departure 또는 destination이 변경될 때 region 업데이트
+    useEffect(() => {
+        if (departure) {
+            setRegion({
+                latitude: departure.latitude,
+                longitude: departure.longitude,
+                latitudeDelta: 0.005,
+                longitudeDelta: 0.005,
+            });
+        }
+    }, [departure]);
+
+    useEffect(() => {
+        if (destination) {
+            setRegion({
+                latitude: destination.latitude,
+                longitude: destination.longitude,
+                latitudeDelta: 0.005,
+                longitudeDelta: 0.005,
+            });
+        }
+    }, [destination]);
+
     return (
         <St.Container>
             <SearchBar
-                setRegion={setRegion}
-                setCurrentAddress={setCurrentAddress}
+                placeholder={departurePlaceholder}
+                onPress={setDeparture}
             />
-            <CustomMap region={region} />
+            <SearchBar
+                placeholder="목적지를 입력하세요"
+                onPress={setDestination}
+            />
+            <CustomMap
+                region={region}
+                departure={departure}
+                destination={destination}
+            />
+            <St.FloatingButtonBottomRight>
+                <IconButton
+                    iconname={'location'}
+                    isRed={true}
+                    onPress={() => {
+                        setRegion((prevRegion) => ({
+                            // 역삼 멀티캠퍼스로 임시 설정
+                            latitude: 37.50127843458193,
+                            longitude: 127.0396046598167,
+                            latitudeDelta: prevRegion?.latitudeDelta || 0.005,
+                            longitudeDelta: prevRegion?.longitudeDelta || 0.005,
+                        }));
+                    }}
+                />
+            </St.FloatingButtonBottomRight>
         </St.Container>
     );
 };
