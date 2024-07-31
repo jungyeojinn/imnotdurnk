8f65b95b4cfbaa243747ec5c7f072430a25f4f1b
이나연 <sh05060@naver.com>
2024-07-31 09:07:43 +0900

    S11P12A609-223 chore : import 오류 제거

---
+++ imnotdurnk_frontend_web/src/hooks/useEmail.jsx
@@ -0,0 +1,12 @@
+//파일이름 바꿀수도? 아직 고민
+
+//용도 1. 해당 이메일이 있는지 확인
+//용도 2. 있으면 이메일에 새 비밀번호 전송
+export const useSendNewPassword = () => {
+    console.log('새 비밀번호 발급 버튼');
+};
+
+//용도 1. 인증번호 전송, 재전송
+export const useSendCertificationNumber = () => {
+    console.log('인증번호 보내기');
+};
--- imnotdurnk_frontend_web/src/hooks/useSendNewPassword.jsx
+++
@@ -1,7 +0,0 @@
-//용도 1. 해당 이메일이 있는지 확인
-//용도 2. 있으면 이메일에 새 비밀번호 전송
-const useSendNewPassword = () => {
-    console.log('새 비밀번호 발급 버튼');
-};
-
-export default useSendNewPassword;
