91bdd207d7ddd1774b56de8fb467c294e8eaef9f
박다솔 <ds10x2@gmail.com>
2024-08-02 16:00:48 +0900

    S11P12A609-254 feat: 발음 평가 결과 계산, 게임 결과를 DB에 저장, 임시 파일 삭제 시점 수정, 음성 파일을 S3에 업로드 구현

--- imnotdurnk_backend/src/main/java/com/imnotdurnk/domain/gamelog/service/VoiceService.java
+++ imnotdurnk_backend/src/main/java/com/imnotdurnk/domain/gamelog/service/VoiceService.java
@@ -1,6 +1,7 @@
 package com.imnotdurnk.domain.gamelog.service;
 
 import com.imnotdurnk.domain.gamelog.dto.VoiceDto;
+import com.imnotdurnk.domain.gamelog.dto.VoiceResultDto;
 import com.imnotdurnk.domain.gamelog.entity.GameLogEntity;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,11 +17,16 @@ public interface VoiceService {
 
     boolean removeVoiceByLogId(int logId);
 
-    int getScoreFromVoice(MultipartFile file) throws IOException, IllegalAccessException, UnsupportedAudioFileException;
+    VoiceResultDto getScoreFromVoice(MultipartFile file) throws IOException, IllegalAccessException, UnsupportedAudioFileException;
 
     File multipartToFile(MultipartFile mfile) throws IOException;
 
     void deleteTempFile(File file) throws IOException;
 
+    int calculateScore(double score);
+
+    void saveVoiceFile(String filename, GameLogEntity gameLogEntity);
+
     String getScriptFromVoice(MultipartFile file) throws IOException, IllegalAccessException, UnsupportedAudioFileException;
+
 }
