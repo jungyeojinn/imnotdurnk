e50ce47f9c694be39fc66edf6975104c710542e6
박다솔 <padasol@naver.com>
2024-08-06 13:57:40 +0900

    S11P12A609-287 fix: https 변경 후 포트번호 삭제

--- imnotdurnk_frontend_web/src/services/api.js
+++ imnotdurnk_frontend_web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://i11a609.p.ssafy.io:8080/api',
+    baseURL: 'https://i11a609.p.ssafy.io/api',
     timeout: 5000, // 5초 내 서버 응답 없으면 요청 취소
     withCredentials: true, // 이게 있어야 refresh Token받을 수 있음
     //zustand로 저장된 accessToken 불러와서 매 요청의 헤더에 넣기
