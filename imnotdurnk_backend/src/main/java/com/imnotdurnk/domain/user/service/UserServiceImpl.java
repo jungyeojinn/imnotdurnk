a576e7a550587501fcbc54b78c4a1537a4c6c396
yngbao97 <yukyj0418@gmail.com>
2024-08-08 14:13:44 +0900

    fix: 이메일 인증시 오류 메시지 세분화

--- imnotdurnk_backend/src/main/java/com/imnotdurnk/domain/user/service/UserServiceImpl.java
+++ imnotdurnk_backend/src/main/java/com/imnotdurnk/domain/user/service/UserServiceImpl.java
@@ -274,10 +274,10 @@ public class UserServiceImpl implements UserService {
             return false;
         }
         if (redisUtil.getData(verificationCode).equals(email)){  //인증코드와 이메일이 일치함
-            redisUtil.setData(email,"0");
+            redisUtil.setData(email,"1");
             return true;
         } else {  //코드와 이메일이 일치하지 않음
-            return false;
+            throw new BadRequestException("이메일 일치하지 않음");
         }
     }
 
