dca2f3631dd3813307e1741bf891e14fa1e4abdb
godeeyeon <godeeyeon@gmail.com>
2024-08-09 17:30:13 +0900

    S11P12A609-364 feat: 발음 게임 - 게임 결과 UI(게임 저장 관련 ConfirmModal 분기 완료), 게임 결과 등록할 일정 리스트 UI(오늘의 일정 리스트 렌더링, 날짜 제외)
    - need: 게임 저장 관련 ConfirmModal 분기 별 함수 작업 추가
    - need: 게임 결과 등록할 일정 리스트에 날짜 띄우기 위해 EventCard 컴포넌트 대공사 필요..

---
+++ imnotdurnk_frontend_web/src/components/game/AddGameToPlan.jsx
@@ -0,0 +1,90 @@
+import { useQuery } from '@tanstack/react-query';
+import { useEffect } from 'react';
+import {
+    convertDateToString,
+    dateStringToUrl,
+} from '../../hooks/useDateTimeFormatter';
+import { getDailyEventList } from '../../services/calendar';
+import useGameStore from '../../stores/useGameStore';
+import useNavigationStore from '../../stores/useNavigationStore';
+import CalendarStatusBar from '../calendar/CalendarStatusBar';
+import EventCard from '../calendar/EventCard';
+import * as St from './AddGameToPlan.style';
+
+const AddGameToPlan = () => {
+    const { voiceGameResult } = useGameStore();
+    const setNavigation = useNavigationStore((state) => state.setNavigation);
+
+    const date = dateStringToUrl(convertDateToString(new Date()));
+
+    const {
+        data: dailyEventList,
+        error,
+        isError,
+        isLoading,
+    } = useQuery({
+        queryKey: ['dailyEventList', date],
+        queryFn: () => getDailyEventList({ date }),
+        enabled: !!date, // date 있을 때만 쿼리 실행
+        keepPreviousData: true, // 새 데이터 가져오는 동안 이전 데이터 유지
+    });
+
+    useEffect(() => {
+        console.log('일정 리스트에서 받아온 voiceGameResult', voiceGameResult);
+        setNavigation({
+            isVisible: true,
+            icon1: { iconname: 'backarrow', path: '-1' },
+            title: '오늘의 일정 목록',
+            icon2: { iconname: 'empty' },
+        });
+    }, []);
+
+    useEffect(() => {
+        console.log('오늘의 일정 가져와 보자 dailyEventList', dailyEventList);
+    }, [dailyEventList]);
+
+    return (
+        <St.CalendarListContainer>
+            <CalendarStatusBar />
+            <St.CalendarListBox>
+                {isLoading ? (
+                    <St.LoadingAndErrorText>
+                        일정을 불러오는 중입니다.
+                    </St.LoadingAndErrorText>
+                ) : isError ? (
+                    <St.LoadingAndErrorText>
+                        Error: {error.message}
+                    </St.LoadingAndErrorText>
+                ) : dailyEventList && dailyEventList.length > 0 ? (
+                    dailyEventList.map((e) => (
+                        <EventCard
+                            key={e.id}
+                            alcoholLevel={e.alcoholLevel}
+                            fromCalendar={true}
+                            eventId={e.id}
+                        >
+                            <div>
+                                <St.CalendarItemTitle
+                                    $alcoholLevel={e.alcoholLevel}
+                                >
+                                    {e.title}
+                                </St.CalendarItemTitle>
+                                <St.CalendarItemTime
+                                    $alcoholLevel={e.alcoholLevel}
+                                >
+                                    {e.time}
+                                </St.CalendarItemTime>
+                            </div>
+                        </EventCard>
+                    ))
+                ) : (
+                    <St.StyledEmptyEvent>
+                        일정이 존재하지 않습니다.
+                    </St.StyledEmptyEvent>
+                )}
+            </St.CalendarListBox>
+        </St.CalendarListContainer>
+    );
+};
+
+export default AddGameToPlan;
---
+++ imnotdurnk_frontend_web/src/components/game/VoiceGameResult.jsx
@@ -0,0 +1,107 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { styled } from 'styled-components';
+import useGameStore from '../../stores/useGameStore';
+import useNavigationStore from '../../stores/useNavigationStore';
+import Button from '../_button/Button';
+import { InfoConfirmModal, ToastSuccess, ToastWarning } from '../_common/alert';
+
+const VoiceGameResult = () => {
+    const navigate = useNavigate();
+    const setNavigation = useNavigationStore((state) => state.setNavigation);
+
+    const { voiceGameResult } = useGameStore();
+
+    useEffect(() => {
+        console.log('게임 결과에서 받아온 voiceGameResult', voiceGameResult);
+        setNavigation({
+            isVisible: true,
+            icon1: { iconname: 'backarrow', path: '/game/voicegame' },
+            title: '발음 평가 결과',
+            icon2: { iconname: 'empty' },
+        });
+    }, []);
+
+    // TODO:
+    const handleSubmit = () => {
+        InfoConfirmModal(
+            '오늘 등록한 일정이 있나요?',
+            '예',
+            '아니오',
+            () => {
+                ToastSuccess('오늘의 일정 리스트로 이동합니다.');
+                navigate('/game/voicegame/result/add-to-plan');
+            },
+            () => {
+                InfoConfirmModal(
+                    '지금 술을 드시고 계신가요?',
+                    '예',
+                    '아니오',
+                    () => {
+                        InfoConfirmModal(
+                            `게임 기록을 저장하려면 일정이 필요합니다. <br/> 
+                            새로운 일정을 생성할까요?`,
+                            '예',
+                            '아니오',
+                            () => {
+                                ToastSuccess('일정 등록 페이지로');
+                            },
+                            () => {
+                                ToastWarning(
+                                    '기록을 저장하려면 일정이 필요합니다.',
+                                );
+                            },
+                            // '게임 기록이 등록 되었습니다.',
+                        );
+                    },
+                    () => {
+                        ToastWarning('술을 마신 경우에만 기록이 저장됩니다.');
+                    },
+                    // '게임 기록이 등록 되었습니다.',
+                );
+            },
+            // '게임 기록이 등록 되었습니다.',
+        );
+    };
+
+    const goToGameList = () => {
+        // TODO: 저장하지 않음을 알리는 API 요청 필요 (파일 명과 함께)
+        navigate('/game');
+    };
+
+    return (
+        <ResultContainer>
+            <h3>당신의 발음 평가 점수는 {voiceGameResult.score}점 입니다.</h3>
+            <ButtonDiv>
+                <Button
+                    text="게임 기록 저장하기"
+                    isRed={false}
+                    border={true}
+                    onClick={handleSubmit}
+                />
+                <Button
+                    text="게임 목록으로"
+                    isRed={true}
+                    onClick={goToGameList}
+                />
+            </ButtonDiv>
+        </ResultContainer>
+    );
+};
+
+export default VoiceGameResult;
+
+const ResultContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 3rem;
+`;
+
+const ButtonDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+
+    width: 100%;
+`;
