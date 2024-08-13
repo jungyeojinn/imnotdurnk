be60e857fac88d0b904357af9883ae8a74bcc030
godeeyeon <godeeyeon@gmail.com>
2024-08-13 14:20:30 +0900

    S11P12A609-378 feat: 게임 기록 저장(발음 게임, 타이핑 게임) 및 잡다한 오류 수정
    - done: 게임 기록 오늘의 일정에 추가 후 일정 상세 실시간 업데이트 처리(쿼리 무효화)
    - done: 오늘 날짜 포함 이전으로만 캘린더 음주 기록 박스 보이도록 수정(등록, 수정, 상세)
    - done: 캘린더 나갈 때 selectedDate 오늘로 설정하여, 게임 일정 등록 시 오늘로 레더링 되도록 처리

--- imnotdurnk_frontend_web/src/components/calendar/PlanDetailAlcohol.jsx
+++ imnotdurnk_frontend_web/src/components/calendar/PlanDetailAlcohol.jsx
@@ -1,9 +1,14 @@
 import { alcoholLevelToString } from '../../hooks/useAlcoholLevelFormatter';
-import { formatTime } from '../../hooks/useDateTimeFormatter';
+import {
+    convertDateToString,
+    formatTime,
+    parseDateTime,
+} from '../../hooks/useDateTimeFormatter';
 import { icons } from '../../shared/constants/icons';
 import * as St from './PlanDetailAlcohol.style';
 
 const PlanDetailAlcohol = ({
+    planDetailDate,
     sojuBottle,
     sojuGlass,
     beerBottle,
@@ -13,47 +18,68 @@ const PlanDetailAlcohol = ({
 }) => {
     const arrivalTimeString = arrivalTime ? formatTime(arrivalTime) : '-';
 
+    const today = parseDateTime(
+        convertDateToString(new Date()),
+        '오전 12시 00분',
+    );
+
+    const formattedPlanDetailDate = `${planDetailDate.split('T')[0]}T00:00`;
+
+    // // 오늘 이전(오늘 포함)에 등록하는 경우에만 알코올 관련 항목 표시
+    const shouldRenderAlcoholComponent = formattedPlanDetailDate <= today;
+
     return (
         <St.AlcoholContainer>
             <St.AlcoholTitle>음주 기록</St.AlcoholTitle>
             <St.InputContainer>
-                <St.DrinkInputBox>
-                    <St.InputItemBox $alcoholCount={true}>
-                        <St.AlcoholCountImage
-                            src={icons['miniSojuBottle']}
-                            alt="soju"
-                            $isSoju={true}
-                        />
-                        <h4>
-                            {sojuBottle}병 {sojuGlass}잔
-                        </h4>
-                    </St.InputItemBox>
-                    <St.InputItemBox $alcoholCount={true}>
-                        <St.AlcoholCountImage
-                            src={icons['miniBeerBottle']}
-                            alt="beer"
-                            $isSoju={false}
-                        />
-                        <h4>
-                            {beerBottle}병 {beerGlass}잔
-                        </h4>
-                    </St.InputItemBox>
-                </St.DrinkInputBox>
+                {shouldRenderAlcoholComponent ? (
+                    <>
+                        <St.DrinkInputBox>
+                            <St.InputItemBox $alcoholCount={true}>
+                                <St.AlcoholCountImage
+                                    src={icons['miniSojuBottle']}
+                                    alt="soju"
+                                    $isSoju={true}
+                                />
+                                <h4>
+                                    {sojuBottle}병 {sojuGlass}잔
+                                </h4>
+                            </St.InputItemBox>
+                            <St.InputItemBox $alcoholCount={true}>
+                                <St.AlcoholCountImage
+                                    src={icons['miniBeerBottle']}
+                                    alt="beer"
+                                    $isSoju={false}
+                                />
+                                <h4>
+                                    {beerBottle}병 {beerGlass}잔
+                                </h4>
+                            </St.InputItemBox>
+                        </St.DrinkInputBox>
 
-                <St.InputItemBox>
-                    <St.InputItemBoxTitle>
-                        <img src={icons['health']} alt="alcohol-level" />
-                        <h4>만취 정도</h4>
-                    </St.InputItemBoxTitle>
-                    <h4>{alcoholLevelToString(alcoholLevel)}</h4>
-                </St.InputItemBox>
-                <St.InputItemBox>
-                    <St.InputItemBoxTitle>
-                        <img src={icons['clock']} alt="arrival-time" />
-                        <h4>귀가 시간</h4>
-                    </St.InputItemBoxTitle>
-                    <h4>{arrivalTimeString}</h4>
-                </St.InputItemBox>
+                        <St.InputItemBox>
+                            <St.InputItemBoxTitle>
+                                <img
+                                    src={icons['health']}
+                                    alt="alcohol-level"
+                                />
+                                <h4>만취 정도</h4>
+                            </St.InputItemBoxTitle>
+                            <h4>{alcoholLevelToString(alcoholLevel)}</h4>
+                        </St.InputItemBox>
+                        <St.InputItemBox>
+                            <St.InputItemBoxTitle>
+                                <img src={icons['clock']} alt="arrival-time" />
+                                <h4>귀가 시간</h4>
+                            </St.InputItemBoxTitle>
+                            <h4>{arrivalTimeString}</h4>
+                        </St.InputItemBox>
+                    </>
+                ) : (
+                    <St.NoDrinkRecord>
+                        아직 음주 일정 전입니다.
+                    </St.NoDrinkRecord>
+                )}
             </St.InputContainer>
         </St.AlcoholContainer>
     );
