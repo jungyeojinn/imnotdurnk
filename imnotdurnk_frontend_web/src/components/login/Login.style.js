8ddc2e97f8e76d7fd587da1c68057549afe50a52
godeeyeon <godeeyeon@gmail.com>
2024-08-14 13:13:30 +0900

    S11P12A609-388 fix: 로그인 페이지에 HOME 버튼 추가

--- imnotdurnk_frontend_web/src/components/login/Login.style.js
+++ imnotdurnk_frontend_web/src/components/login/Login.style.js
@@ -1,6 +1,6 @@
 import { styled } from 'styled-components';
 
-export const LoginContainer = styled.div`
+const LoginContainer = styled.div`
     display: flex;
     width: 23.5rem;
     padding: 0rem 2.3571rem;
@@ -9,7 +9,8 @@ export const LoginContainer = styled.div`
     align-items: flex-start;
     gap: 1.3571rem;
 `;
-export const TitleContainer = styled.div`
+
+const TitleContainer = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -17,7 +18,8 @@ export const TitleContainer = styled.div`
     gap: 0.7143rem;
     align-self: stretch;
 `;
-export const FormContainer = styled.form`
+
+const FormContainer = styled.form`
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -25,16 +27,18 @@ export const FormContainer = styled.form`
     gap: 1.3571rem;
     align-self: stretch;
 `;
-export const SubTitle = styled.h3`
-    color: var(--color-green3, #252f2c);
-    line-height: normal;
-`;
-export const MainTitle = styled.h1`
+
+const SubTitle = styled.h3`
     color: var(--color-green3, #252f2c);
     line-height: normal;
 `;
 
-export const InputBoxWrapper = styled.div`
+const MainTitle = styled.h1`
+    color: var(--color-green3, #252f2c);
+    line-height: normal;
+`;
+
+const InputBoxWrapper = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -42,17 +46,39 @@ export const InputBoxWrapper = styled.div`
     gap: 0.6429rem;
     align-self: stretch;
 `;
-export const LoginSubQuestionContainer = styled.div`
+
+const LoginSubQuestionContainer = styled.div`
     display: flex;
     width: 18.7857rem;
     height: 1.1429rem;
     justify-content: space-between;
     align-items: center;
 `;
-export const ForgetPasswordMessage = styled.h6`
+
+const ForgetPasswordMessage = styled.h6`
     color: var(--color-green3, #252f2c);
     text-align: right;
     font-size: var(--font-body-h6, 0.57rem);
     line-height: normal;
     cursor: pointer;
 `;
+
+const ButtonBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 0.7143rem;
+
+    width: 100%;
+`;
+
+export {
+    ButtonBox,
+    ForgetPasswordMessage,
+    FormContainer,
+    InputBoxWrapper,
+    LoginContainer,
+    LoginSubQuestionContainer,
+    MainTitle,
+    SubTitle,
+    TitleContainer,
+};
